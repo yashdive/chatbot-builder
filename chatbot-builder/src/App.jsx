3f808b2b5fb5cc5a8c316dd21b9156d4964badb3
Yash Dive <yashdive190@gmail.com>
2025-08-01 11:25:55 +0530

    Created client structure

---
+++ chatbot-builder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-200 text-3xl font-bold shadow-2xl">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
