3be6308ac566667ae4b29d43360bbb54214f3da9
Yash Dive <yashdive190@gmail.com>
2025-08-05 01:29:58 +0530

    Deploy 2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  viteStaticCopy({
-      targets: [
-        {
-          src: 'index.html',
-          dest: '.' // copies index.html to dist/404.html
-        }
-      ]
-    })],
+    tailwindcss()],
     base:'/chatbot-builder/'
 })
