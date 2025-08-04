f00ebd79180339b26a19dbc94227e271fcbb3630
Yash Dive <yashdive190@gmail.com>
2025-08-05 01:33:17 +0530

    Deploy 3

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <Router basename='"/chatbot-builder"'>
+  <Router basename='/chatbot-builder'>
     <App />
   </Router>,
 )
